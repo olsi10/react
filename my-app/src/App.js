988705c9133a5247ca02d172e5b7a5d16f50ffef
olsi10 <wangbampang2035@gmail.com>
2023-01-04 23:57:12 +0900

    웹 브라우저 라우팅 -> 웹 페이지 이동 구현

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,29 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        {/* a 태그의 href="/" 와 같음 */}
+        <Link to = "/">Home</Link> | <Link to = "/about">About</Link>
+      </nav>
+      {/* Routes는 Route의 묶음 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
         <Home />
     </div>
   );
 }
+
 
 export default App;
 
