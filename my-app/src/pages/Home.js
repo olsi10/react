64d01acb50ff8917d3f3b2f3da897d2133d80de0
olsi10 <wangbampang2035@gmail.com>
2023-01-04 23:56:59 +0900

    내브바 컴포넌트 생성

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,18 +1,21 @@
 // jsx 문법을 사용하기 위해서 react import
 // import React from "react";
 
-import React, {Component} from "react";
+// import React, {Component} from "react";
 
 // 리액트 컴포넌트 [함수형, 클래스형]
-
-function Home() {
-    return <h1>Home 화면입니다.</h1>
-}
 
 // class Home extends Component {
 //     render() {
 //         return <h1>Home 화면입니다.</h1>
 //     }
 // }
+
+import React from "react";
+
+const Home = () => {
+    return <h1>Home 화면입니다.</h1>
+};
+
 
 export default Home;
