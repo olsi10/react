c7be3c5c717266095ba17d70d2c9cf9c02c48b12
olsi10 <wangbampang2035@gmail.com>
2023-01-04 23:38:30 +0900

    컴포넌트 만들기

---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,18 @@
+// jsx 문법을 사용하기 위해서 react import
+// import React from "react";
+
+import React, {Component} from "react";
+
+// 리액트 컴포넌트 [함수형, 클래스형]
+
+function Home() {
+    return <h1>Home 화면입니다.</h1>
+}
+
+// class Home extends Component {
+//     render() {
+//         return <h1>Home 화면입니다.</h1>
+//     }
+// }
+
+export default Home;
