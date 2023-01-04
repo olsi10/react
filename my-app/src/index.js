eef13ab7c41d501de6944f51c9d58473544fbefa
olsi10 <wangbampang2035@gmail.com>
2023-01-04 23:30:23 +0900

    엄격모드 / 웹 바이탈

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+  // 자바스크립트의 [엄격모드]를 쓰겠다.
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +16,7 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// 웹 바이탈이라는 라이브러리가 있음
+// 웹 바이탈 = 웹 퍼포먼스 측정할 때
 reportWebVitals();
