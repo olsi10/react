a84be357624e86c711ae43c691b7c45db6e5c5ad
olsi10 <wangbampang2035@gmail.com>
2023-01-05 23:46:49 +0900

    컴포넌트 적용, css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,20 +6,28 @@ import {Routes, Route, Link} from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Counter from "./pages/Counter";
+import Input from "./pages/Input";
+import Input2 from "./pages/Input2";
+import List from "./pages/List";
+
 
 function App() {
   return (
     <div className="App">
       <nav>
         {/* a 태그의 href="/" 와 같음 */}
-        <Link to = "/">Home</Link> | <Link to = "/about">About</Link>
+        <Link to = "/">Home</Link> | <Link to = "/about">About</Link> | <Link to = "/counter">Counter</Link> | <Link to = "/input">Input</Link> | <Link to = "/input2">Input2</Link> | <Link to = "/input">Input</Link> | <Link to = "/list">List</Link> 
       </nav>
       {/* Routes는 Route의 묶음 */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input/>} />
+        <Route path="/input2" element={<Input2/>} />
+        <Route path="/list" element={<List/>} />
       </Routes>
-        <Home />
     </div>
   );
 }
